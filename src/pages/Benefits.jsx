5a48a54f9b247e3237ea4c736dbd0eb7f1035c03
SattvaDoshi <sattva103@gmail.com>
2024-07-11 17:04:54 +0530

    made Changes

--- src/pages/Benefits.jsx
+++ src/pages/Benefits.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import TedTalk from '../components/benefits/TedTalk'
-import Podcast from '../components/benefits/TedTalk'
-import Resume from '../components/benefits/TedTalk'
+import Podcast from '../components/benefits/Podcast'
+import Resume from '../components/benefits/Resume'
 
 const Benefits = () => {
   return (
